fd75b6846fd49dcb34422a102f6a903269a9a306
qb1968 <qb1968@aol.com>
2021-03-17 14:25:35 -0400

    birthday project

---
+++ birthday/src/data.js
@@ -0,0 +1,25 @@
+const data =  [
+  {
+    id: 1,
+    name: 'Batman',
+    age: 52,
+    image:
+      'https://res.cloudinary.com/qb1968/image/upload/v1601309777/ipuskssepkhvutsrhhol.jpg',
+  },
+  {
+    id: 2,
+    name: 'Joker',
+    age: 55,
+    image:
+      'https://res.cloudinary.com/qb1968/image/upload/v1613015927/trydicyfsix2d59kue3p.jpg',
+  },
+  {
+    id: 3,
+    name: 'Superman',
+    age: 75,
+    image:
+      'https://res.cloudinary.com/qb1968/image/upload/v1616004995/zaxt3ntnhwf6pixzpu0n.jpg',
+    }
+]
+
+export default data
