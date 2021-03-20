edd026f86a228807577dfbdc80f8156f83ba8d9e
qb1968 <qb1968@aol.com>
2021-03-20 18:22:53 -0400

    stripe

---
+++ stripe_submenus/src/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from './images/logo.svg'
+import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context';
+
+const Navbar = () => {
+    const { openSidebar, openSideMenu, closeSubMenu } = useGlobalContext()
+    const displaySubmenu = (e) => {
+        const page = e.target.textContent
+        const tempBtn = e.target.getBoundingClientRect
+        const center = (tempBtn.left + tempBtn.right) / 2
+        const bottom = tempBtn.bottom - 3
+        openSubmenu(page, { center, bottom })
+    }
+    const handleSubmenu = (e) => {
+        if (!e.target.classList.contains('link-btn')) {
+            closeSubMenu()
+        }
+    }
+    return (
+        <nav className='nav' onMouseOver={handleSubmit}>
+            <div className='nav-center'>
+                <div className='nav-header'>
+                    <img src={logo} className='nav-logo' alt='' />
+                    <button className='btn toggle-btn' onClick={openSidebar}>
+                        <FaBars/>
+                    </button>
+                </div>
+                <ul className='nav-links'>
+                    <li>
+                        <button className='link-btn' onMouseOver={displaySubmenu}>
+                            Products
+                        </button>
+                    </li>
+                    <li>
+                        <button className='link-btn' onMouseOver={displaySubmenu}>
+                            Developers
+                        </button>
+                    </li>
+                    <li>
+                        <button className='link-btn' onMouseOver={displaySubmenu}>
+                            Company
+                        </button>
+                    </li>
+                </ul>
+                <button className='btn signin-btn'>Sign In</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
