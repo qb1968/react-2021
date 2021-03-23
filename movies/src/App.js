e581e489d66d91abac6b32b290e9e6c20e5a9991
qb1968 <qb1968@aol.com>
2021-03-22 23:34:15 -0400

    movies

---
+++ movies/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Movie from './components/SingleMovie'
+
+const App = () => {
+  return (
+    <Switch>
+      <Route path='/' exact>
+        <Home/>
+      </Route>
+      <Route path='/movies/:id' children={<Movie/>}/>
+    </Switch>
+  )
+}
+
+export default App
